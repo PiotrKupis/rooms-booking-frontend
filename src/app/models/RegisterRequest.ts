ab24819ca27f69442606fbb58a75a37f47deb1ba
Piotr Kupis <piotr.kupis42@gmail.com>
2021-09-29 15:42:31 +0200

    ET-7 add dto for registering a new user

---
+++ src/app/models/RegisterRequest.ts
@@ -0,0 +1,9 @@
+export interface RegisterRequest {
+  name: string;
+  surname: string;
+  password: string;
+  repeatedPassword: string;
+  email: string;
+  countryCode: string;
+  phoneNumber: string;
+}
