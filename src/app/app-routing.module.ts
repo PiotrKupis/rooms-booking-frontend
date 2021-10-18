ab16a7b82d8223bf3494e95c95587cbafc05c5d2
Piotr Kupis <piotr.kupis42@gmail.com>
2021-10-18 09:39:58 +0200

    ET-4 add resort payload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home/home.component";
 import {RegistrationComponent} from "./components/registration/registration.component";
 import {LoginComponent} from "./components/login/login.component";
+import {AddResortComponent} from "./components/add-resort/add-resort.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'registration', component: RegistrationComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'add-resort', component: AddResortComponent}
 ];
 
 @NgModule({
---
+++ src/app/models/add-resort-payload.ts
@@ -0,0 +1,16 @@
+export interface AddResortPayload {
+  resortName: string;
+  country: string;
+  city: string;
+  street: string,
+  streetNumber: string;
+  resortAmenities: Array<string>;
+  smokingPermitted: boolean;
+  animalsPermitted: boolean;
+  partyPermitted: boolean;
+  hotelDayStart: string;
+  hotelDayEnd: string;
+  isParkingAvailable: boolean;
+  parkingFee: string;
+  parkingFeeCurrency: string;
+}
