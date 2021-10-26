7e01719e298fc8774baad2f2352a4ca5e6943f35
Piotr Kupis <piotr.kupis42@gmail.com>
2021-10-26 19:56:18 +0200

    add room service

---
+++ src/app/services/room.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {AddRoomRequest} from "../models/addRoomRequest";
+import {Observable} from "rxjs";
+import {RoomPayload} from "../models/roomPayload";
+import {environment} from "../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoomService {
+
+  path = environment.apiEndpoint + "room";
+
+  constructor(private http: HttpClient) {
+  }
+
+  addRoom(addRoomRequest: AddRoomRequest): Observable<RoomPayload> {
+    return this.http.post<RoomPayload>(this.path, addRoomRequest);
+  }
+}
