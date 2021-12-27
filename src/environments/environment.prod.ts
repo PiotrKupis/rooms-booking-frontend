f6d750b9cb443557ee76c2c0d48a4681589196e1
Piotr Kupis <piotr.kupis42@gmail.com>
2021-12-27 14:16:38 +0100

    add missing properties for prod configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiEndpoint: 'https://prime-room-backend.herokuapp.com/'
+  apiEndpoint: 'https://prime-room-backend.herokuapp.com/',
+  defaultCountry: 'Polska',
+  defaultCurrency: 'PLN'
 };
